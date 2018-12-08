fc925b8238ea5fa96ea18941679c4caebf83b6c6
Geecek <gicalamaciej@gmail.com>
2018-12-08 23:09:57 +0100

    Add todays APOD's fetching

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import { View, Text, Platform } from 'react-native';
+import {
+  View, Text, Platform, StyleSheet, Image,
+} from 'react-native';
 
 import TabBarIcon from '../components/TabBarIcon';
+import Layout from '../constants/Layout';
+
+import config from '../config/config';
+
+const styles = StyleSheet.create({
+  header: {
+    textAlign: 'center',
+    fontSize: 28,
+    marginTop: 40,
+  },
+  caption: {
+    textAlign: 'center',
+  },
+});
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -11,10 +27,42 @@ export default class HomeScreen extends React.Component {
     ),
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      todaysAPOD: null,
+    };
+  }
+
+  async componentDidMount() {
+    // eslint-disable-next-line no-undef
+    const { _bodyInit } = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${config.API_KEY}`,
+    );
+    const todaysAPOD = JSON.parse(_bodyInit);
+    this.setState({ todaysAPOD });
+  }
+
   render() {
+    const { todaysAPOD } = this.state;
+
+    if (todaysAPOD) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.header}>Browse Astronomy pictures of the day!</Text>
+          <Image
+            style={{ width: Layout.window.width, height: 300 }}
+            source={{ uri: todaysAPOD.url }}
+          />
+          <Text style={styles.caption}>{todaysAPOD.title}</Text>
+          <Text style={styles.caption}>{todaysAPOD.copyright}</Text>
+          <Text style={styles.caption}>{todaysAPOD.date}</Text>
+        </View>
+      );
+    }
     return (
       <View>
-        <Text>Home Screen</Text>
+        <Text style={styles.header}>Browse Astronomy pictures of the day!</Text>
       </View>
     );
   }
